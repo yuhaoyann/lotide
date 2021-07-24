a0c35aca6f6322015814235906972a878b947682
yuhaoyann <yuhaoyann@gmail.com>
2021-07-24 11:51:56 -0400

    new head

--- head.js
+++ head.js
@@ -14,11 +14,16 @@ const assertEqual = function(actual, expected) {
     } else {
       console.log(`${fail}${fail}${fail}Assertion Failed: ${actual} !== ${expected}`);
     }
-  } else {
-    console.log('error');
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+const head = function(array) {
+  if (array[0]) {
+    return array[0];
+  } else {
+    console.log(array[0]);
+  }
+}
+
+assertEqual(head([]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
