c3e64332238c3fec184d3197a72069d1997a19c6
yuhaoyann <yuhaoyann@gmail.com>
2021-07-26 23:16:46 -0400

    get first element

--- head.js
+++ head.js
@@ -14,16 +14,14 @@ const assertEqual = function(actual, expected) {
     } else {
       console.log(`${fail}${fail}${fail}Assertion Failed: ${actual} !== ${expected}`);
     }
+  } else {
+    console.log('error');
   }
 };
 
 const head = function(array) {
-  if (array[0]) {
-    return array[0];
-  } else {
-    console.log(array[0]);
-  }
-}
+  return array[0];
+};
 
 assertEqual(head([]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
