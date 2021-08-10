a06060aedea0c143dbce2f56936ca33905bf201b
yuhaoyann <yuhaoyann@gmail.com>
2021-08-10 10:35:58 -0400

    use recursion to make eqObjects working for nested objects

--- eqObjects.js
+++ eqObjects.js
@@ -18,22 +18,19 @@
 
 const eqArrays = require('./eqArrays');
 
-// const eqArrays = function(array1, array2) {
-//   for (let i = 0; i < (array1.length + array2.length) / 2; i++) {
-//     if (array1[i] !== array2[i]) {
-//       return false;
-//     }
-//   }
-//   return true;
-// };
-
 const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length === Object.keys(object2).length) {
     for (let key in object1) {
-      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-        if (!eqArrays(object1[key], object2[key])) {
-          return false;
-        }
+      if (typeof object1[key] === 'object' && typeof object2[key] === 'object') {
+        if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+          if (!eqArrays(object1[key], object2[key])) {
+            return false;
+          }
+        } else if (!Array.isArray(object1[key]) && !Array.isArray(object2[key])) {
+            if (!eqObjects(object1[key], object2[key])) {
+              return false;
+            }
+          }
       } else if (object1[key] !== object2[key]) {
         return false;
       }
@@ -44,6 +41,12 @@ const eqObjects = function(object1, object2) {
 };
 
 module.exports = eqObjects;
+
+// console.log(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })); // => true
+// console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })); // => false
+// console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 })); // => false
+// console.log(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 22 }));
+
 // const ab = { a: "1", b: "2" };
 // const ba = { b: "2", a: "1" };
 // const abc = { a: "1", b: "2", c: "3" };
