2574b4c5ec0f9e724080f98b213f941f4acff684
yuhaoyann <yuhaoyann@gmail.com>
2021-08-10 11:02:59 -0400

    updated for nested arrays

--- eqArrays.js
+++ eqArrays.js
@@ -17,13 +17,25 @@
 // };
 
 const eqArrays = function(array1, array2) {
-  for (let i = 0; i < (array1.length + array2.length) / 2; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (Array.isArray(array1[i]) && Array.isArray(array2[i])) {
+        if (eqArrays(array1[i], array2[i]) === false) {
+          return false;
+        }
+      } else if (array1[i] !== array2[i]) {
+          return false;
+        }
     }
+    return true;
   }
-  return true;
+  return false;  
 };
+
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])); // => true
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])); // => false
+console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])); // => false
+
 module.exports = eqArrays;
 // assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 // assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
