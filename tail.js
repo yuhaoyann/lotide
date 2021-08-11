5d6b9438849422a0bba526fc7dca6037a8a9f07f
yuhaoyann <yuhaoyann@gmail.com>
2021-08-11 17:55:45 -0400

    moved test file for tail to test folder

--- tail.js
+++ tail.js
@@ -1,21 +1,3 @@
-// const assertEqual = function(actual, expected) {
-//   let pass = String.fromCodePoint(0x1F493);
-//   let fail = String.fromCodePoint(0x1F4A3);
-//   if (typeof actual === 'string' && typeof actual === typeof expected) {
-//     if (actual === expected) {
-//       console.log(`${pass}${pass}${pass}Assertion Passed: "${actual}" === "${expected}"`);
-//     } else {
-//       console.log(`${fail}${fail}${fail}Assertion Failed: "${actual}" !== "${expected}"`);
-//     }
-//   } else {
-//     if (actual === expected) {
-//       console.log(`${pass}${pass}${pass}Assertion Passed: ${actual} === ${expected}`);
-//     } else {
-//       console.log(`${fail}${fail}${fail}Assertion Failed: ${actual} !== ${expected}`);
-//     }
-//   }
-// };
-
 const assertEqual = require('./assertEqual');
 
 const tail = function(array) {
@@ -26,4 +8,4 @@ const tail = function(array) {
   return out;
 };
 
-assertEqual(tail([]), 5);
+module.exports = tail;
---
+++ test/tailTest.js
@@ -0,0 +1,4 @@
+let tail = require('../tail');
+let assertEqual = require('../assertEqual');
+
+assertEqual(tail([]), 5);
