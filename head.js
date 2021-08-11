1656ce55902c5e755ac885bc2f698286de92831b
yuhaoyann <yuhaoyann@gmail.com>
2021-08-11 17:51:47 -0400

    moved test code to test folder

--- head.js
+++ head.js
@@ -1,26 +1,7 @@
-// const assertEqual = function(actual, expected) {
-//   let pass = String.fromCodePoint(0x1F493);
-//   let fail = String.fromCodePoint(0x1F4A3);
-//   if (typeof actual === 'string' && typeof actual === typeof expected) {
-//     if (actual === expected) {
-//       console.log(`${pass}${pass}${pass}Assertion Passed: "${actual}" === "${expected}"`);
-//     } else {
-//       console.log(`${fail}${fail}${fail}Assertion Failed: "${actual}" !== "${expected}"`);
-//     }
-//   } else {
-//     if (actual === expected) {
-//       console.log(`${pass}${pass}${pass}Assertion Passed: ${actual} === ${expected}`);
-//     } else {
-//       console.log(`${fail}${fail}${fail}Assertion Failed: ${actual} !== ${expected}`);
-//     }
-//   }
-// };
-
 const assertEqual = require('./assertEqual');
 
 const head = function(array) {
   return array[0];
 };
 
-assertEqual(head([]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
