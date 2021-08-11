915060e7ff02b48250c3bd0f11113e264fc4c94b
yuhaoyann <yuhaoyann@gmail.com>
2021-08-11 18:27:28 -0400

    refactored middle.js moving test away

--- middle.js
+++ middle.js
@@ -1,23 +1,3 @@
-// const assertArraysEqual = function(array1, array2) {
-//   const eqArrays = function(array1, array2) {
-//     for (let i = 0; i < (array1.length + array2.length) / 2; i++) {
-//       if (array1[i] !== array2[i]) {
-//         return false;
-//       }
-//     }
-//     return true;
-//   };
-//   let pass = String.fromCodePoint(0x1F493);
-//   let fail = String.fromCodePoint(0x1F4A3);
-//   if (eqArrays(array1, array2) === true) {
-//     console.log(`${pass}${pass}${pass}Assertion Passed: [${array1}] === [${array2}]`);
-//   } else {
-//     console.log(`${fail}${fail}${fail}Assertion Failed: [${array1}] !== [${array2}]`);
-//   }
-// };
-
-const assertArraysEqual = require('./assertArraysEqual');
-
 const middle = function(array) {
   let out = [];
   let middle = Math.floor(array.length / 2);
@@ -31,4 +11,5 @@ const middle = function(array) {
   }
   return out;
 };
-assertArraysEqual(middle([1,2,3,4,5,6]), [3,4]);
+
+module.exports = middle;
---
+++ test/middleTest.js
@@ -0,0 +1,4 @@
+const assertArraysEqual = require('../assertArraysEqual');
+const middle = require('../middle');
+
+assertArraysEqual(middle([1,2,3,4,5,6]), [3,4]);
