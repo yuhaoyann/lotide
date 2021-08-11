ddcf747947b7f19f9f22ab839f115ee5b890fd49
yuhaoyann <yuhaoyann@gmail.com>
2021-08-11 18:08:46 -0400

    refactored assertArraysEqual to move test to test folder

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,6 @@
+let assertArraysEqual = require('../assertArraysEqual');
+
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
